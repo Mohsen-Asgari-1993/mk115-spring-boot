22851bb3b98ff6cb67ce5f31b78ee066f1d58d93
maxixking66 <maxixking66@gamil.com>
2024-09-19 14:16:21 +0330

    add example for profiles

--- src/main/java/ir/maktabsharif115/springboot/Application.java
+++ src/main/java/ir/maktabsharif115/springboot/Application.java
@@ -1,22 +1,26 @@
 package ir.maktabsharif115.springboot;
 
 import ir.maktabsharif115.springboot.service.MyService;
-import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-        EntityManager entityManager = run.getBean(EntityManager.class);
-        System.out.println("done");
+
+        System.out.println(
+                run.getBean(MyService.class).getUsername()
+        );
+
     }
 
     @Bean
+    @Profile(value = {"dev"})
     public MyService myService() {
         return new MyService();
     }
--- src/main/java/ir/maktabsharif115/springboot/service/MyService.java
+++ src/main/java/ir/maktabsharif115/springboot/service/MyService.java
@@ -1,4 +1,13 @@
 package ir.maktabsharif115.springboot.service;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+
+@Getter
+@Setter
 public class MyService {
+
+    @Value("${my-username}")
+    private String username;
 }
