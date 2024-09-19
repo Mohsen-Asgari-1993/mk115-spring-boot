4a1e589f227000c0ee85e32aa1d1a605c306e7bf
maxixking66 <maxixking66@gamil.com>
2024-09-19 11:39:28 +0330

    init project

---
+++ src/test/java/ir/maktabsharif115/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif115.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
