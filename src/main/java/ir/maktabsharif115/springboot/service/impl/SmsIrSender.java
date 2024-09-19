f73cf266ef7e8ac9c43255a0c06c238ebb42e6e3
maxixking66 <maxixking66@gamil.com>
2024-09-19 14:33:44 +0330

    use @ConfigurationProperties example 2

--- src/main/java/ir/maktabsharif115/springboot/service/impl/SmsIrSender.java
+++ src/main/java/ir/maktabsharif115/springboot/service/impl/SmsIrSender.java
@@ -1,15 +1,18 @@
 package ir.maktabsharif115.springboot.service.impl;
 
-import ir.maktabsharif115.springboot.config.SmsIrConfig;
 import ir.maktabsharif115.springboot.service.SmsSender;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SmsIrSender implements SmsSender {
 
-    private final SmsIrConfig config;
+    private final Config config;
 
     @Override
     public void send(String mobileNumber, String content) {
@@ -18,6 +21,23 @@ public class SmsIrSender implements SmsSender {
         System.out.println("url: " + config.getUrl());
         System.out.println("token: " + config.getToken());
         System.out.println("callBackUrl: " + config.getCallBackUrl());
+    }
+
+    @Component
+    @ConfigurationProperties(prefix = "sms.sms-ir")
+    @Getter
+    @Setter
+    public static class Config {
+
+        private String username;
+
+        private String password;
+
+        private String url;
+
+        private String token;
+
+        private String callBackUrl;
     }
 
 }
