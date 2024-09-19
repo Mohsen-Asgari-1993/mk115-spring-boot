c5af6eaea445d7ec91f48e49f70c3ff9cb4a5195
maxixking66 <maxixking66@gamil.com>
2024-09-19 11:56:12 +0330

    add banner and properties for datasource

--- src/main/java/ir/maktabsharif115/springboot/Application.java
+++ src/main/java/ir/maktabsharif115/springboot/Application.java
@@ -1,13 +1,24 @@
 package ir.maktabsharif115.springboot;
 
+import ir.maktabsharif115.springboot.service.MyService;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+        EntityManager entityManager = run.getBean(EntityManager.class);
+        System.out.println("done");
+    }
+
+    @Bean
+    public MyService myService() {
+        return new MyService();
     }
 
 }
