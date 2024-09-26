9b85d3be12dacc1a3f3bfbf96490f92c0df83ee1
maxixking66 <maxixking66@gamil.com>
2024-09-26 16:46:37 +0330

    use ResponseEntity

--- src/main/java/ir/maktabsharif115/springboot/contorller/CategoryResource.java
+++ src/main/java/ir/maktabsharif115/springboot/contorller/CategoryResource.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,23 +27,25 @@ public class CategoryResource {
     private final CategoryService categoryService;
 
     @PostMapping
-    public CategoryDTO create(@RequestBody @Valid CategoryCreationDTO dto) {
+    public ResponseEntity<CategoryDTO> create(@RequestBody @Valid CategoryCreationDTO dto) {
         Category category = categoryService.create(dto);
-        return new CategoryDTO(
-                category.getId(),
-                category.getTitle(),
-                category.getParent() != null ?
-                        CategoryDTO.builder()
-                                .id(category.getId())
-                                .title(category.getTitle())
-                                .parent(
-                                        category.getParent() != null ?
-                                                CategoryDTO.builder()
-                                                        .id(category.getParent().getId())
-                                                        .title(category.getParent().getTitle())
-                                                        .build() : null
-                                ).build()
-                        : null
+        return ResponseEntity.ok(
+                new CategoryDTO(
+                        category.getId(),
+                        category.getTitle(),
+                        category.getParent() != null ?
+                                CategoryDTO.builder()
+                                        .id(category.getId())
+                                        .title(category.getTitle())
+                                        .parent(
+                                                category.getParent() != null ?
+                                                        CategoryDTO.builder()
+                                                                .id(category.getParent().getId())
+                                                                .title(category.getParent().getTitle())
+                                                                .build() : null
+                                        ).build()
+                                : null
+                )
         );
     }
 
