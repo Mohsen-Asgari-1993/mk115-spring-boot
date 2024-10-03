0f2894bd29e4c008c2f8a7f005003eedaf32b197
maxixking66 <maxixking66@gamil.com>
2024-10-03 11:47:23 +0330

    remove extra classes and files

--- src/main/java/ir/maktabsharif115/springboot/contorller/CategoryResource.java
+++ src/main/java/ir/maktabsharif115/springboot/contorller/CategoryResource.java
@@ -1,25 +1,17 @@
 package ir.maktabsharif115.springboot.contorller;
 
 import ir.maktabsharif115.springboot.domain.Category;
-import ir.maktabsharif115.springboot.exceptions.GeneralRuntimeException;
 import ir.maktabsharif115.springboot.mapper.CategoryMapper;
 import ir.maktabsharif115.springboot.service.CategoryService;
 import ir.maktabsharif115.springboot.service.dto.CategoryCreationDTO;
 import ir.maktabsharif115.springboot.service.dto.CategoryDTO;
 import ir.maktabsharif115.springboot.service.dto.CategoryUpdateDTO;
-import ir.maktabsharif115.springboot.validators.AllowedValues;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.Serializable;
-import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
@@ -40,26 +32,6 @@ public class CategoryResource {
                 httpHeaders,
                 HttpStatus.CREATED
         );
-        /*return new ResponseEntity<>(
-                new CategoryDTO(
-                        category.getId(),
-                        category.getTitle(),
-                        category.getParent() != null ?
-                                CategoryDTO.builder()
-                                        .id(category.getId())
-                                        .title(category.getTitle())
-                                        .parent(
-                                                category.getParent() != null ?
-                                                        CategoryDTO.builder()
-                                                                .id(category.getParent().getId())
-                                                                .title(category.getParent().getTitle())
-                                                                .build() : null
-                                        ).build()
-                                : null
-                ),
-                httpHeaders,
-                HttpStatus.CREATED
-        );*/
     }
 
 
@@ -81,69 +53,5 @@ public class CategoryResource {
                         categoryService.findById(id)
                 )
         );
-    }
-
-    @PostMapping("/validation")
-    public void validate(@RequestBody @Valid ValidationDTO dto) {
-
-    }
-
-    @GetMapping("/exception/run-time")
-    public void getRuntimeException() {
-        throw new RuntimeException("this is message");
-    }
-
-    @GetMapping("/exception/custom")
-    public void getCustomException() {
-        throw new GeneralRuntimeException("this is custom message", HttpStatus.PRECONDITION_FAILED);
-    }
-
-    @Setter
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ValidationDTO implements Serializable {
-
-        @NotNull
-        private Long id;
-
-        @Min(value = 50)
-        private Long parentId;
-
-        @Size(min = 2, max = 3)
-        private List<String> names;
-
-        @AllowedValues(required = false, values = {"1", "2"}, message = "only '1', '2' is allowed")
-        private String type;
-
-        @AllowedValues(values = {"1", "2"}, message = "only '1', '2' is allowed")
-        private String model;
-
-        @AllowedValues(values = {"RED", "BLUE"}, message = "only 'RED', 'BLUE' is allowed")
-        private List<String> colors;
-
-        @Valid
-        private ValidationDetailDTO dto;
-
-        @Valid
-//        private List<@Valid ValidationDetailDTO> detailList;
-        private List<ValidationDetailDTO> detailList;
-    }
-
-    @Setter
-    @Getter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class ValidationDetailDTO implements Serializable {
-
-        @NotNull
-        private Long id;
-
-        @Min(value = 10)
-        @NotNull
-        private Long parentId;
-
-        @Size(min = 2, max = 3)
-        private List<String> names;
     }
 }
