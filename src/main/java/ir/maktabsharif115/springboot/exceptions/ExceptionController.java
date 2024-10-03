3301814be043d69740ec7753860f82caf9f85b58
maxixking66 <maxixking66@gamil.com>
2024-10-03 10:14:16 +0330

    add ResponseEntityExceptionHandler

--- src/main/java/ir/maktabsharif115/springboot/exceptions/ExceptionController.java
+++ src/main/java/ir/maktabsharif115/springboot/exceptions/ExceptionController.java
@@ -4,18 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 //@RestControllerAdvice
-public class ExceptionController /*extends ResponseEntityExceptionHandler*/ {
+public class ExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value = {RuntimeException.class})
     public ModelAndView getRuntimeException(RuntimeException ex) {
@@ -34,6 +45,56 @@ public class ExceptionController /*extends ResponseEntityExceptionHandler*/ {
                 ),
                 ex.getHttpStatus()
         );
+    }
+
+    /*@ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> getMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", HttpStatus.BAD_REQUEST);
+
+        //Get all errors
+        List<FieldErrorDTO> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> new FieldErrorDTO(fieldError.getField(), fieldError.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+
+    }*/
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatusCode status,
+                                                                  WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", HttpStatus.BAD_REQUEST);
+
+        //Get all errors
+        List<FieldErrorDTO> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(fieldError -> new FieldErrorDTO(fieldError.getField(), fieldError.getDefaultMessage()))
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+        return new ResponseEntity<>(
+                body, headers, status
+        );
+    }
+
+    @Setter
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FieldErrorDTO implements Serializable {
+        private String field;
+        private String message;
     }
 
 
