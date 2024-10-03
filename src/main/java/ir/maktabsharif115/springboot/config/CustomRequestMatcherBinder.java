da63b3243780ea2cce2505d3cd67a951a301612e
maxixking66 <maxixking66@gamil.com>
2024-10-03 16:47:47 +0330

    add CustomRequestMatcherBinder

---
+++ src/main/java/ir/maktabsharif115/springboot/config/CustomRequestMatcherBinder.java
@@ -0,0 +1,38 @@
+package ir.maktabsharif115.springboot.config;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class CustomRequestMatcherBinder implements RequestMatchersBinder {
+
+    @Override
+    public String[] getPermitAllUrls() {
+        return new String[]{
+                "/user/register"
+        };
+    }
+
+    @Override
+    public Collection<RequestMatcherAuthorityPair> getAuthorityPairs() {
+        return List.of(
+                new RequestMatcherAuthorityPair() {
+                    @Override
+                    public String[] getUrls() {
+                        return new String[]{
+                                "/admin/**"
+                        };
+                    }
+
+                    @Override
+                    public String[] getAuthorities() {
+                        return new String[]{
+                                "admin"
+                        };
+                    }
+                }
+        );
+    }
+}
